3753f0f4980df3cdbcc5d190150bb40a487aa4bd
Chen Yefan <cyefan2@gmail.com>
2025-08-07 02:00:40 +0800

    提交最新更改

---
+++ mario.py
@@ -0,0 +1,6 @@
+# 第1步：获取用户输入（多少层砖块？）
+n = int(input("Height: "))
+
+# 第2步：用循环打印 n 个 #
+for i in range(n):
+    print("#")
---
+++ meow.py
@@ -0,0 +1,12 @@
+
+
+
+def meow():
+        print("meow")
+
+def main():
+    for i in range(3):
+        meow()
+
+if __name__ == "__main__":
+    main()
